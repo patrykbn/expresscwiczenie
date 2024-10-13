25a15381232b90a11990d4a3deca140590e9aa6b
Patryk Bagan <patrykbn1997@gmail.com>
2024-10-13 15:26:18 +0200

    Handlebars

--- server.js
+++ server.js
@@ -1,35 +1,46 @@
 const express = require('express');
 const path = require('path');
+const hbs = require('express-handlebars');
 
 const app = express();
+app.engine('.hbs', hbs());
+app.set('view engine', '.hbs');
 
 app.use(express.static(path.join(__dirname, '/public')));
 
 app.use((req, res, next) => {
-    res.show = (name) => {
-      res.sendFile(path.join(__dirname, `/views/${name}`));
-    };
-    next();
-});
-
-app.use((req, res, next) => {
     if (req.path.startsWith('/user')) {
-        res.show('/forbidden.html')
+        res.render('/forbidden.hbs')
     } else {
         next();
     }
 });
 
+app.get('/hello/:name', (req, res) => {
+    res.render('hello', {name: req.params.name});
+});
+
 app.get('/', (req, res) => {
-    res.show('home.html');
+    res.render('home');
 });
 
 app.get('/home', (req, res) => {
-    res.show('home.html');
+    res.render('home');
 });
 
 app.get('/about', (req, res) => {
-    res.show('about.html');
+    res.render('about');
+});
+
+app.get('/history', (req, res) => {
+    res.render('history');
+});
+app.get('/info', (req, res) => {
+    res.render('info', { layout: 'dark'});
+});
+
+app.get('/contact', (req, res) => {
+    res.render('contact');
 });
 
 app.get('/style.css', (req, res) => {
@@ -42,7 +53,7 @@ app.get('/style.css', (req, res) => {
 
 
 app.use((req, res) => {
-    res.status(404).show('/404.html');
+    res.status(404).render('404', { layout: false });
 });
 
 app.listen(8000, () => {
